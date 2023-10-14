5bcf12c149cc6de9f058be9f259b943558a2dfcf
Lev Logvinov <llogvinov356@gmail.com>
2023-10-14 18:37:38 +0300

    Added spawners on test scene

--- Assets/Scripts/Main/TrashObjectSpawner.cs
+++ Assets/Scripts/Main/TrashObjectSpawner.cs
@@ -7,12 +7,18 @@ namespace Main
     public class TrashObjectSpawner : MonoBehaviour
     {
         [SerializeField] private TrashData _trashData;
+        [SerializeField] int _count = 10;
+        [Space] 
+        [SerializeField] private float _sideBound;
+        [SerializeField] private float _topBound;
+        [SerializeField] private float _bottomBound;
 
         private List<TrashObject> _trashObjects;
 
         private void Awake()
         {
             _trashObjects = new List<TrashObject>();
+            Spawn(_count);
         }
 
         public void Spawn(int count)
@@ -28,7 +34,9 @@ namespace Main
             Instantiate(_trashData.Model, GetRandomPosition(), GetRandomRotation(), transform);
 
         private Vector3 GetRandomPosition() =>
-            new Vector3(0, 0, 0);
+            new (Random.Range(-_sideBound, _sideBound),
+                Random.Range(1f, 2f),
+                Random.Range(_bottomBound, _topBound));
 
         private Quaternion GetRandomRotation() =>
             Random.rotation;
