2b859e0b251b686f6044fbeff95704cbc46cb03b
Lev Logvinov <llogvinov356@gmail.com>
2023-11-18 15:47:46 +0300

    Added animation on start drag trash object

--- Assets/Scripts/Main/TrashObject.cs
+++ Assets/Scripts/Main/TrashObject.cs
@@ -1,16 +1,21 @@
-﻿using ObjectsData;
+﻿using DG.Tweening;
+using ObjectsData;
 using UnityEngine;
 
 namespace Main
 {
     public class TrashObject : MonoBehaviour
     {
+        [SerializeField] private Vector3 _dragRotation;
+
         public TrashData TrashData { get; private set; }
         public bool IsDragged { get; private set; }
         public bool IsThrown { get; set; }
         
         private Rigidbody _rigidbody;
         private Collider _collider;
+
+        private Tween _rotateTween;
 
         public void Init(TrashData trashData)
         {
@@ -28,10 +33,19 @@ namespace Main
             _rigidbody.angularVelocity = Vector3.zero;
             _rigidbody.useGravity = false;
             _rigidbody.isKinematic = true;
+
+            RotateObject();
+        }
+
+        private void RotateObject()
+        {
+            _rotateTween = transform.DORotate(_dragRotation, 0.5f);
         }
 
         public void OnEndDrag()
         {
+            _rotateTween?.Kill();
+
             IsDragged = false;
             _rigidbody.useGravity = true;
             _rigidbody.isKinematic = false;
