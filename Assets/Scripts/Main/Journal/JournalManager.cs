1d994643b9d8b3591a3697792a15ef7981c2651c
Lev Logvinov <llogvinov356@gmail.com>
2023-12-12 12:40:00 +0300

    Added simple journal of trash objects

---
+++ Assets/Scripts/Main/Journal/JournalManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ObjectsData;
+
+namespace Main.Journal
+{
+    public static class JournalManager
+    {
+        private static Dictionary<TrashCanData, List<TrashData>> _journal;
+
+        public static Dictionary<TrashCanData, List<TrashData>> GetJournal()
+        {
+            if (_journal is not null && _journal.Count > 0) 
+                return _journal;
+            
+            _journal = new Dictionary<TrashCanData, List<TrashData>>();
+            foreach (var trashCanData in ResourceLoader.TrashCanDatas)
+            {
+                _journal.TryAdd(trashCanData,
+                    ResourceLoader.TrashDatas.Where(d => d.Type == trashCanData.Type).ToList());
+            }
+            return _journal;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIJournal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using Main.Journal;
+using UI.Base;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIJournal : UIBase
+    {
+        [SerializeField] private Text _text;
+        [SerializeField] private Button _button;
+
+        private void OnEnable() => 
+            _button.onClick.AddListener(GetJournal);
+
+        private void OnDisable() => 
+            _button.onClick.RemoveListener(GetJournal);
+
+        private void GetJournal()
+        {
+            var journal = JournalManager.GetJournal();
+            var stringBuilder = new StringBuilder();
+            foreach (var journalKey in journal.Keys)
+            {
+                stringBuilder.Append(journalKey.Type);
+                if (journal.TryGetValue(journalKey, out var trashDatas))
+                {
+                    foreach (var trashData in trashDatas)
+                    {
+                        stringBuilder.Append($"\n\t{trashData.Title}");
+                    }
+                    stringBuilder.Append("\n");
+                }
+            }
+
+            _text.text = stringBuilder.ToString();
+        }
+    }
+}
