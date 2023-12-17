0a612e5a46569e63217aceec59f9c2e2ec571e72
Lev Logvinov <llogvinov356@gmail.com>
2023-12-18 00:54:07 +0300

    Updated recycle animation

--- Assets/Scripts/Main/RecycleManager.cs
+++ Assets/Scripts/Main/RecycleManager.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Collections;
 using Core.Audio;
 using DG.Tweening;
 using UnityEngine;
@@ -29,7 +29,7 @@ namespace Main
 
         private void OnSuccess(TrashObject trashObject, TrashCan trashCan)
         {
-            DoAnimation(trashObject, trashCan, () =>
+            DisposeAnimation(trashObject, trashCan, onComplete: () =>
             {
                 _throwTrashAudioPlayer.Switch(play: true);
                 trashCan.FXDust.Play();
@@ -38,38 +38,46 @@ namespace Main
 
         private void OnFail(TrashObject trashObject)
         {
-            var trashCans = FindObjectsOfType<TrashCan>();
-            var trashCan = trashCans.FirstOrDefault(
-                c => c.TrashCanData.Type == trashObject.TrashData.Type);
-            if (trashCan == null) return;
-            
             if (Timer.HasInstance)
                 Timer.Instance.ReduceTime(5f); // todo: change this
-            
-            DoAnimation(trashObject, trashCan);
-            _throwAwayAudioPlayer.Switch(play: true);
+
+            PushToCenter(trashObject,
+                onStart: () => _throwAwayAudioPlayer.Switch(play: true),
+                onComplete: () =>
+                {
+                    trashObject.IsThrown = false;
+                    trashObject.ToggleInteraction(true);
+                });
         }
 
-        private static void DoAnimation(TrashObject trashObject, TrashCan trashCan, Action onFinish = default)
+        private static void DisposeAnimation(TrashObject trashObject, TrashCan trashCan, Action onComplete = default)
         {
             const float animationDurationPart = AnimationDuration / 2;
             const float midScaler = 0.5f;
 
             var animationTween = DOTween.Sequence();
-            animationTween.Append(trashObject.transform.DOMove(trashCan.ObjectStartPoint.position, animationDurationPart));
+            animationTween.Append(trashObject.transform.DOMove(trashCan.ObjectStartPoint.position, animationDurationPart).SetEase(Ease.OutQuint));
             animationTween.Insert(animationDurationPart,
                 trashObject.transform.DOScale(Vector3.one * midScaler, animationDurationPart).SetEase(Ease.OutQuint));
-            animationTween.Append(trashObject.transform.DOMove(trashCan.ObjectEndPoint.position, animationDurationPart));
+            animationTween.Append(trashObject.transform.DOMove(trashCan.ObjectEndPoint.position, animationDurationPart).SetEase(Ease.InExpo));
             animationTween.Insert(animationDurationPart,
-                trashObject.transform.DOScale(Vector3.zero, animationDurationPart));
+                trashObject.transform.DOScale(Vector3.zero, animationDurationPart).SetEase(Ease.InExpo));
             animationTween.OnComplete(() =>
             {
-                onFinish?.Invoke();
+                onComplete?.Invoke();
                 trashObject.gameObject.SetActive(false);
                 trashObject.TrashObjectSpawner.ObjectThrown(trashObject);
                 if (trashObject.TrashObjectSpawner.AllObjectsThrown)
                     AllObjectsOfSpawnerThrown?.Invoke();
             });
+        }
+
+        private void PushToCenter(TrashObject trashObject, Action onStart = default, Action onComplete = default)
+        {
+            trashObject.transform
+                .DOMove(Vector3.up * 4f, AnimationDuration)
+                .OnStart(() => onStart?.Invoke())
+                .OnComplete(() => onComplete?.Invoke());
         }
     }
 }
