19d40b4efcf064273dbfb284e9f624e17eb699d5
Lev Logvinov <llogvinov356@gmail.com>
2025-03-29 13:43:57 +0300

    Added powerup scripts

---
+++ Assets/Scripts/PowerUps/ActivatedPowerUps/ActivatedPowerUp.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PowerUps.ActivatedPowerUps
+{
+    public abstract class ActivatedPowerUp : IPowerUp
+    {
+        public Action Applied;
+
+        public abstract void Apply();
+    }
+}
---
+++ Assets/Scripts/PowerUps/TimePowerUps/TimePowerUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PowerUps.TimePowerUps
+{
+    public abstract class TimePowerUp : IPowerUp
+    {
+        public static event Action<TimePowerUp, float> AnyTimePowerUpStarted;
+        
+        public event Action Started;
+        public event Action<TimePowerUp> Finished;
+        
+        protected readonly float Duration;
+
+        public TimePowerUp(float duration)
+        {
+            Duration = duration;
+
+            Started += OnStarted;
+            Finished += OnFinished;
+        }
+
+        protected abstract void OnStarted();
+        protected abstract void OnFinished(TimePowerUp powerUp);
+
+        public async Task Activate()
+        {
+            AnyTimePowerUpStarted?.Invoke(this, Duration);
+            Started?.Invoke();
+            await Task.Delay((int) (Duration * 1000));
+            Finished?.Invoke(this);
+        }
+        
+        ~TimePowerUp()
+        {
+            Started -= OnStarted;
+            Finished -= OnFinished;
+        }
+    }
+}
